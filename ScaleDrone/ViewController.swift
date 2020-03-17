2fd4330ef04c92e883e7ae60c64d8e599a3cc462
arkrayBergonia <fbergonia.arkray@gmail.com>
2020-03-17 20:44:05 +0800

    Initial Commit

---
+++ ScaleDrone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScaleDrone
+//
+//  Created by Francis Jemuel Bergonia on 3/17/20.
+//  Copyright © 2020 Xi Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
